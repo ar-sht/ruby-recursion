45581269d26885bb1b05116616a58e7b8fb917b5
Ari Shtein <ashtein120@gmail.com>
2022-09-28 21:14:34 -0400

    Add a normal loop fibonacci function

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -10,4 +10,19 @@ def fibs(n)
   arr
 end
 
-print fibs(200)
+print fibs(20) // test
+
+outside_arr = []
+def fibs_rec(n)
+  if n < 2
+    outside_arr << n
+    n
+  end
+
+  num = fibs_rec(n - 1) + fibs_rec(n - 2)
+  outside_arr << num
+  num
+end
+
+fibs_rec(20)
+print outside_arr
